b12f1c6915313199110ce1cfe23059d8d208a480
Katherine O'Connell <koconnel@chicagobooth.edu>
2023-05-02 16:29:40 +0000

    first routing error fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-  get("/", { :controller => "users", :action => "home" })
+  get("/", { :controller => "users", :action => "index" }) #changed home to index
   get("/users", { :controller => "users", :action => "all_users" })
   get("/users/:username", { :controller => "user", :action => "show" })
   get("/insert_user_record", { :controller => "venues", :action => "create" })
@@ -11,7 +10,6 @@ Rails.application.routes.draw do
   get("/venue/:an_id", { :controller => "venue", :action => "show" })
   post("/update_venue/:the_id", { :controller => "application", :action => "update" })
   get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
-  
+
   get("/insert_comment_record", { :controller => "comments", :action => "create" })
- 
 end
